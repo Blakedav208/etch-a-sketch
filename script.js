ca9c7d6f59f543ebe839eade7369a5e3170ec137
Blake Davis <Blakedav2088@gmail.com>
2020-12-14 02:51:39 -0500

    added functionality to change the ciolor of each div to blue, and added a clear button to the page

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-const mainDiv = document.querySelector('.boardContainer');
+function makeDivs(size) {
+  const mainDiv = document.querySelector(".boardContainer");
 
-for(let i = 0; i < 256; i++){
-    const newDiv = document.createElement('div');
-    newDiv.classList.add('boardDiv');
+  for (let i = 0; i < size; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("boardDiv");
     mainDiv.appendChild(newDiv);
-}
+  }
+} //end of make divs
+
+makeDivs(256);
+
+const divs = document.querySelectorAll(".boardDiv");
+
+divs.forEach((div) => {
+  div.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "blue";
+  });
+});
+
+const clearBtn = document.querySelector(".clearGrid");
+
+clearBtn.addEventListener("click", () => {
+  divs.forEach((div) => {
+      div.style.backgroundColor = "white";
+  });
+  
+});
