dc5044f45a2eb5795c6aedab0e3238e97338b918
Blake Davis <Blakedav2088@gmail.com>
2020-12-15 01:35:33 -0500

    added functinality to resize the grid when you hit the clear grid button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function makeDivs(size) {
-  const mainDiv = document.querySelector(".boardContainer");
 
+
+const mainDiv = document.querySelector(".boardContainer");
+function makeDivs(size) {
   for (let i = 0; i < size; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("boardDiv");
@@ -12,17 +13,27 @@ makeDivs(256);
 
 const divs = document.querySelectorAll(".boardDiv");
 
-divs.forEach((div) => {
-  div.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "blue";
+function addColorEffect(nodeList) {
+  nodeList.forEach((div) => {
+    div.addEventListener("mouseover", (e) => {
+      div.style.backgroundColor = "blue";
+    });
   });
-});
+}
+
+addColorEffect(divs);
 
 const clearBtn = document.querySelector(".clearGrid");
 
 clearBtn.addEventListener("click", () => {
   divs.forEach((div) => {
-      div.style.backgroundColor = "white";
+    div.style.backgroundColor = "white";
   });
-  
+
+  let newGridSize = parseInt(prompt("New Grid Size?:"));
+  mainDiv.style.gridTemplateColumns = `repeat(${newGridSize}, 20px)`;
+  mainDiv.style.gridTemplateRows = `repeat(${newGridSize}, 20px)`;
+  makeDivs(newGridSize * newGridSize);
+  const newDivs = document.querySelectorAll(".boardDiv");
+  addColorEffect(newDivs);
 });
